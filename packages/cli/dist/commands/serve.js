c637d1255f6209e6ffc259c1ad35031fe7a87210
Ronan <binnyronan@gmail.com>
2021-07-08 20:23:26 +1200

    Handling relative file paths

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,13 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 var commander_1 = require("commander");
 var local_api_1 = require("local-api");
+var posix_1 = __importDefault(require("path/posix"));
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p,--port <number>', 'port to run server on', '4005')
     .action(function (filename, options) {
     if (filename === void 0) { filename = 'notebook.js'; }
-    local_api_1.serve(parseInt(options.port), filename, '/');
+    var dir = posix_1.default.join(process.cwd(), posix_1.default.dirname(filename));
+    local_api_1.serve(parseInt(options.port), posix_1.default.basename(filename), dir);
 });
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,10 +1,12 @@
 import { Command } from 'commander';
 import { serve } from 'local-api';
+import path from 'path/posix';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p,--port <number>', 'port to run server on', '4005')
   .action((filename = 'notebook.js', options: { port: string }) => {
-    serve(parseInt(options.port), filename, '/');
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
