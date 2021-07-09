222fafbaa7ad77b233e080718539afdd5b0c3da1
Ronan <binnyronan@gmail.com>
2021-07-09 23:01:58 +1200

    setup proxy to handle whether the app is run in the dev or prod environment

--- packages/cli/node_modules/local-api/src/index.ts
+++ packages/cli/node_modules/local-api/src/index.ts
@@ -1,7 +1,29 @@
+import e from 'express';
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
+
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+      })
+    );
+  } else {
+    const packagePath = require.resolve('local-client/build/index.html');
+    console.log(packagePath);
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
