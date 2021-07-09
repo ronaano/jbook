b357bd1fbdf320dbe04383936e95e83d9a45b0ae
Ronan <binnyronan@gmail.com>
2021-07-09 23:24:17 +1200

    Setup initial reading file contents and setting default cell values

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -10,15 +10,24 @@ interface Cell {
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  router.use(express.json());
 
   const fullPath = path.join(dir, filename);
 
   router.get('/cells', async (req, res) => {
-    // Make sure the cell storage file exists
-    // Add default list of cells if doesn't extist
-    // Read the file
-    // Parse a list of cells out of it
-    // Send list of cells back to the browser
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        // Add code to create a file and add default cells
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
   });
 
   router.post('/cells', async (req, res) => {
