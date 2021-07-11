af6e5994dd1d4c7799c1c818a47e3fda665666a9
Ronan <binnyronan@gmail.com>
2021-07-11 23:04:57 +1200

    adding local-client back step 1

---
+++ packages/local-clien/src/components/code-cell.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import Resizable from './resizable';
+import { Cell } from '../state';
+import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/used-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulative-code-selector';
+import './code-cell.css';
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
+
+  useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cumulativeCode);
+    }
+    const timer = setTimeout(async () => {
+      createBundle(cell.id, cumulativeCode);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cumulativeCode, cell.id, createBundle]);
+
+  return (
+    <Resizable direction='vertical'>
+      <div
+        style={{
+          height: 'calc(100% - 10px)',
+          display: 'flex',
+          flexDirection: 'row'
+        }}
+      >
+        <Resizable direction='horizontal'>
+          <CodeEditor
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
+        </Resizable>
+        <div className='wrapper'>
+          {!bundle || bundle.loading ? (
+            <div className='progress-cover'>
+              <progress className='progress is-small is-primary' max='100'>
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundlingStatus={bundle.err} />
+          )}
+        </div>
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
